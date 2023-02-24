830e4db514583f9fc1d0516f0d0a588be6907650
Pooria Azami <pooriaazami@gmail.com>
2023-02-24 12:12:26 +0330

    check win status

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use rand::{rngs::ThreadRng, Rng};
 
-#[derive(Eq, PartialEq)]
+#[derive(Eq, PartialEq, Copy, Clone)]
 enum MapCells {
     BALNK,
     X,
@@ -39,8 +39,92 @@ fn print_game_map(game_map: &Vec<MapCells>) {
     }
 }
 
+fn check_winner_status(game_map: &Vec<MapCells>) -> MapCells {
+    let mut sum: i32 = 0;
+    let mut cell = MapCells::BALNK;
+
+    for i in 0..4 {
+        sum = 0;
+        cell = MapCells::BALNK;
+
+        for j in 0..4 {
+            let idx = i * 4 + j;
+            if cell == game_map[idx] {
+                sum += 1;
+            } else {
+                sum = 1;
+                cell = game_map[idx];
+            }
+        }
+    }
+
+    if cell != MapCells::BALNK && sum == 3 {
+        return cell;
+    }
+
+    for j in 0..4 {
+        sum = 0;
+        cell = MapCells::BALNK;
+
+        for i in 0..4 {
+            let idx = i * 4 + j;
+            if cell == game_map[idx] {
+                sum += 1;
+            } else {
+                sum = 1;
+                cell = game_map[idx];
+            }
+        }
+    }
+
+    if cell != MapCells::BALNK && sum == 3 {
+        return cell;
+    }
+
+    let main_diagonal = vec![0, 5, 10, 15];
+    let anti_diagonal = vec![3, 6, 9, 12];
+
+    sum = 0;
+    cell = MapCells::BALNK;
+    main_diagonal.iter().enumerate().for_each(|(i, _)| {
+        if cell == game_map[main_diagonal[i]] {
+            sum += 1;
+        } else {
+            sum = 1;
+            cell = game_map[main_diagonal[i]];
+        }
+    });
+
+    if cell != MapCells::BALNK && sum == 3 {
+        return cell;
+    }
+
+    sum = 0;
+    cell = MapCells::BALNK;
+    anti_diagonal.iter().enumerate().for_each(|(i, _)| {
+        if cell == game_map[anti_diagonal[i]] {
+            sum += 1;
+        } else {
+            sum = 1;
+            cell = game_map[anti_diagonal[i]];
+        }
+    });
+
+    if cell != MapCells::BALNK && sum == 3 {
+        return cell;
+    }
+
+    return MapCells::BALNK;
+}
+
 fn main() {
     let mut rng = rand::thread_rng();
     let mut game_map = initiate_game_map(&mut rng);
+
     print_game_map(&game_map);
+
+    let mut win_status = check_winner_status(&game_map);
+    while win_status == MapCells::BALNK || win_status == MapCells::LOCK {
+        break;
+    }
 }
